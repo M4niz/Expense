4153bb053d1ed16dbd3347cb8e41c0f922ba8cfa
Manish-Meta <manishmohan.g@metayb.ai>
2026-01-13 16:41:44 +0530

    MergeFIX\

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,36 @@
-import AuthenticateUser from "./pages/AuthenticateUser"
-import SubmitExpense from "./pages/Dashboard/SubmitExpense"
-import EmployeeDashboard from "./pages/Dashboard/EmployeeDashboard"
+import React from "react"
 import Layout from "./pages/Layout"
-
-
-
+import AuthenticateUser from './pages/AuthenticateUser'
+import RoleSelection from "./pages/RoleSelectionBoard"
+ 
+ 
+ 
 import { Routes, Route, Navigate } from "react-router-dom"
-
-
-import RoleSelection from './pages/RoleSelectionBoard'
-
-import EmployeeReports from './pages/reports/EmployeeReports'
+ 
+ 
+import Routters from "./route/Routters"
 // import useGlobalContext from './config/GlobalStateContext'
-
+ 
  localStorage.setItem("login","false");
-
+ 
 const getLoggedIn = localStorage.getItem("login");
-
-
+ 
+ 
 localStorage.setItem("login", "false")
-
+ 
 const App = () => {
+  const [role, setRole] = useState("employee")
   return (
-    <Routes>
-
-     
-      <Route path="/" element={<RoleSelection />} />
-
-   
-      <Route path="/login" element={<AuthenticateUser />} />
-
-     
-      <Route path="/employee" element={<Layout />}>
-        <Route index element={<EmployeeDashboard />} />
-        <Route path="submit" element={<SubmitExpense />} />
-        <Route path="reports" element={<EmployeeReports/>}/>
-      </Route>
-
-=======
-      <Route path="/" element={<RoleSelection/>}/>
-      <Route path="/login" element={<AuthenticateUser/>}/>
-      <Route path="/employee" element={<Layout/>}/>
-      {/* <Route path="/employee/reports" element={<EmployeeReports/>}/> */}
-
-    
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+     <Routes>
+          <Route path="/" element={<RoleSelection/>}/>
+          <Route path="/login" element={<AuthenticateUser/>} />
+          <Route path="/*" element={<Layout />}>
+            <Route path="*" element={<Routters />} />
+          </Route>
+        </Routes>
   )
 }
-
+ 
 export default App
+ 
+ 
